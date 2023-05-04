f7416049b4ac177bfc0feec3950874b59c63467d
tlshyej <tlshyej@gmail.com>
2023-05-04 10:07:58 +0900

    회원가입 관련 테스트 코드 추가

---
+++ shop/src/test/java/orm/jpa/shop/service/MemberServiceImplTest.java
@@ -0,0 +1,67 @@
+package orm.jpa.shop.service;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+import orm.jpa.shop.entity.Address;
+import orm.jpa.shop.entity.Member;
+import orm.jpa.shop.repository.MemberRepository;
+
+@SpringBootTest
+@Transactional
+class MemberServiceImplTest {
+
+    @Autowired private MemberService memberService;
+    @Autowired private MemberRepository memberRepository;
+
+    @Test
+    void signup() {
+
+        Member member = memberService.signup(Member.builder()
+                                                    .memberName("JPA")
+                                                    .build());
+
+        Assertions.assertEquals(member, memberRepository.findById(member.getMemberId()).orElse(null));
+    }
+
+    @Test
+    void signupWithDuplicate() {
+
+        Member member1 = memberService.signup(Member.builder()
+                                                    .memberName("JPA")
+                                                    .build());
+
+        Assertions.assertThrows(IllegalStateException.class, () -> {
+            Member member2 = memberService.signup(Member.builder()
+                                                        .memberName("JPA")
+                                                        .build());
+        });
+    }
+
+    @Test
+    void findByMemberId() {
+        Member member = memberService.signup(Member.builder()
+                                                    .memberName("JPA")
+                                                    .build());
+
+        Member findMember = memberService.findByMemberId(member.getMemberId());
+
+        org.assertj.core.api.Assertions.assertThat(member).isEqualTo(findMember);
+    }
+
+    @Test
+    void findMembers() {
+        Member member1 = memberService.signup(Member.builder()
+                                                    .memberName("JPA")
+                                                    .build());
+
+        Member member2 = memberService.signup(Member.builder()
+                                                    .memberName("JPA1")
+                                                    .build());
+
+        org.assertj.core.api.Assertions.assertThat(memberService.findMembers().size()).isEqualTo(2);
+    }
+
+}
